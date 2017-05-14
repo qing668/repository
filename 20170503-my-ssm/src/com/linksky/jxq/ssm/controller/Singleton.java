8ebf4cd9855421e55d2ab9e6cc03c8fc818782ce
Administrator <Administrator@172.23.243.3>
2017-05-14 16:40:29 +0800

    singleton

--- 20170503-my-ssm/src/com/linksky/jxq/ssm/controller/Singleton.java
+++ 20170503-my-ssm/src/com/linksky/jxq/ssm/controller/Singleton.java
@@ -3,12 +3,16 @@ package com.linksky.jxq.ssm.controller;
 import java.util.Map;
 
 public class Singleton {
-	private static Singleton sin = new Singleton();
+//	private static Singleton sin = new Singleton();  //饿汉模式
+	private static Singleton sin = null;
 	private static Map map;
 	
 	private Singleton(){}
 	
 	public static Singleton getInstance(){
+		if (sin == null){
+			sin = new Singleton();
+		}
 		return sin;
 	}
 
